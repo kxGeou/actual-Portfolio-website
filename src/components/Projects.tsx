3e5f7dae76601df8b0024bae644049f6a952cade
kxGeou <grzegorzm.work@gmail.com>
2025-03-07 23:08:42 +0100

    Project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,50 @@
 import Project from "./ui/Project";
 import Scriptly from '../assets/images/scriptly.png'
+import Theme from '../assets/images/theme.png'
+import Red from '../assets/images/red.png'
+import Todo from '../assets/images/todo.png'
+
+const projectList = [
+    {
+        language : ["Firebase", "Database Fetch", "Design"],
+        image : Scriptly,
+        alternative : "Scriptly hero section",
+        project : "Platforma Edukacyjna",
+        description : "Scriptly to interaktywna platforma edukacyjno-dokumentacyjna, której celem jest nauka podstaw JavaScript oraz umiejętność czytania dokumentacji technicznej. Projekt pomaga początkującym programistom w zrozumieniu kluczowych koncepcji języka oraz nawigowaniu po dokumentacji w efektywny sposób.",
+        link: "https://scriptly-a2c84.web.app/"
+    },
+    {
+        image : Theme,
+        alternative : "Theme Website",
+        project : "Motyw Visual Studio Code",
+        description : "Stworzyłem własny, autorski motyw dla Visual Studio Code, zaprojektowany z myślą o minimalistycznym wyglądzie oraz czytelności kodu. Motyw wykorzystuje odcienie fioletu, nadając edytorowi nowoczesny i estetyczny wygląd, jednocześnie dbając o komfort pracy programisty.",
+        link: "https://minimaldarktheme.netlify.app/"
+    },
+    {
+        image : Todo,
+        alternative : "Todo List website",
+        project : "CRUD Todo lista",
+        description : "Stworzyłem prostą aplikację typu Todo List z funkcjami CRUD (Create, Read, Update, Delete), która pozwala użytkownikowi na zarządzanie listą zadań. Aplikacja zapisuje dane bezpośrednio na urządzeniu użytkownika, co oznacza, że lista zadań pozostaje dostępna nawet po odświeżeniu strony, dzięki wykorzystaniu localStorage.",
+        language : ["localStorage", "CRUD", "Design"],
+        link: "https://tasktrack-typescript.netlify.app/"
+    },
+    {
+        image : Red,
+        alternative : "Red motion website",
+        project : "Strona dla firmy z drukarkami 3D",
+        description : "To był mój pierwszy komercyjny projekt – stworzyłem stronę internetową dla firmy R3d, specjalizującej się w drukarkach 3D. Zajmowałem się zarówno projektowaniem, jak i implementacją całej strony. Zrealizowałem ją w React, dbając o nowoczesny wygląd oraz responsywność, aby strona działała poprawnie na różnych urządzeniach.",
+        language : [ "React", "Design"],
+        link: "https://r3dmotion.com/"
+    },
+]
+
 export default function Projects() {
   return (
     <section>
         <h2 className="font-semibold text-[20px] mb-8">Projekty</h2>
-        <Project languages={["FireBase", "Database"]} image={Scriptly} alternative="scriptly" project="Platforma Edukacyjna" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, voluptatem tenetur nemo sequi aperiam illum eum provident quisquam commodi. Et."></Project>
+        {projectList.map((project, projectIndex) => (
+            <Project key={projectIndex} languages={project.language} image={project.image} project={project.project} link={project.link} description={project.description} alternative={project.alternative}></Project>
+        ))}
     </section>
   )
 }
--- src/components/ui/Project.tsx
+++ src/components/ui/Project.tsx
@@ -1,32 +1,35 @@
+import { Link } from "react-router-dom";
 import Language from "./Language";
 import Title from "./Title";
 import { useState } from "react";
+
 type TProject = {
     project : string;
     description : string; 
     languages?: string[];
     image : any;
     alternative : string ;
+    link : string;
 }
 
-export default function Project({project, description, languages, image, alternative } : TProject) {
+export default function Project({project, description, languages, image, alternative, link } : TProject) {
     const [hover, isHover] = useState<boolean>(false)
   
     return (
-    <div onMouseEnter={() => isHover(!hover)} onMouseLeave={() => isHover(!hover)} className="cursor-pointer ">
-        <Title name={project} isHover={hover}></Title>
-        <p className={`text-not-active text-wrap mt-1 ${languages?.length ? "" : "mb-5"}`}>{description}</p>
-
-        {languages?.length && (
-            <div className="flex gap-3 my-6">
-                {languages?.map((item, itemIndex) => (
-                    <Language name={item} key={itemIndex}></Language>
-                ))}
-            </div>
-            )
-        }
-        
-        <img src={image} alt={alternative} loading="lazy" className={`max-w-[200px] aspect-video object-cover rounded border-2 border-slate-200/10 transition ${hover ? "border-slate-200/30" : ""} sm:order-1 sm:col-span-2 sm:translate-y-1`}/>
-    </div>
+    <Link to={link}>
+        <div onMouseEnter={() => isHover(!hover)} onMouseLeave={() => isHover(!hover)} className="cursor-pointer mb-20">
+            <Title name={project} isHover={hover}></Title>
+            <p className={`text-not-active text-wrap mt-2 ${languages?.length ? "" : "mb-5"}`}>{description}</p>
+            {languages?.length && (
+                <div className="flex gap-3 my-6">
+                    {languages?.map((item, itemIndex) => (
+                        <Language name={item} key={itemIndex}></Language>
+                    ))}
+                </div>
+                )
+            }
+            <img src={image} alt={alternative} loading="lazy" className={`max-w-[200px] aspect-video object-cover rounded border-2 border-slate-200/10 transition ${hover ? "border-slate-200/30" : ""} sm:order-1 sm:col-span-2 sm:translate-y-1`}/>
+        </div>
+    </Link>
   )
 }
