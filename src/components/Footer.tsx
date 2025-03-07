7d22dbd77ed566846f8f46a4e9c726c4ecf9da35
kxGeou <grzegorzm.work@gmail.com>
2025-03-07 23:34:17 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <p className="text-not-active">Strona zdesignowana w programie <span className="text-text">Figma</span>, zakodowana w <span className="text-text">Visual Studio Code </span>. Zbudowana za pomocą <span className="text-text">React</span> i wystylizowana za pomocą <span className="text-text">TailwindCSS</span> </p>
+    </footer>
   )
 }
