a49d68789ff1da5fbe41bd6b7cc29ba459054a02
kxGeou <grzegorzm.work@gmail.com>
2025-03-07 23:25:39 +0100

    Update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,8 @@ import Scriptly from '../assets/images/scriptly.png'
 import Theme from '../assets/images/theme.png'
 import Red from '../assets/images/red.png'
 import Todo from '../assets/images/todo.png'
-
+import { Link } from "react-router-dom";
+import {  useState } from "react";
 const projectList = [
     {
         language : ["Firebase", "Database Fetch", "Design"],
@@ -39,12 +40,19 @@ const projectList = [
 ]
 
 export default function Projects() {
+    const [hover, setHover] = useState<boolean>(false)
   return (
     <section>
         <h2 className="font-semibold text-[20px] mb-8">Projekty</h2>
         {projectList.map((project, projectIndex) => (
             <Project key={projectIndex} languages={project.language} image={project.image} project={project.project} link={project.link} description={project.description} alternative={project.alternative}></Project>
         ))}
+        <p className="font-semibold text-[1.20rem] flex justify-start items-center gap-3 cursor-pointer" onMouseLeave={() => setHover(!hover)} onMouseEnter={() => setHover(!hover)}>
+            <Link to="" className={`transition-all duration-300 ${hover && "border-b-1 border-text-box"}`}>Zobacz wszystkie projekty
+               <i className={`fa-solid fa-arrow-right text-[1rem] ml-2 transition-all duration-200 ${hover && "translate-x-2"}`}></i>
+            </Link>
+            
+        </p>
     </section>
   )
 }
