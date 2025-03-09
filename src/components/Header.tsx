3fed59036dfe2a50838f90c81ee39d859de145aa
kxGeou <grzegorzm.work@gmail.com>
2025-03-09 01:58:37 +0100

    Mini update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
   }
 
   return (
-    <header className="space-y-5 lg:fixed lg:flex lg:flex-col lg:justify-between lg:min-h-[80%]">
+    <header className="space-y-5 lg:fixed lg:flex lg:flex-col lg:justify-between lg:min-h-[80%] ">
         <div className="flex flex-col">
-          <h2 className="text-[2.25rem] font-bold lg:text-[46px]">{HeaderText.name}</h2>
+          <h2 className="text-[2.25rem] font-bold lg:text-[46px] ">{HeaderText.name}</h2>
           <h2 className="text-[1.15rem] font-medium mb-5 lg:text-[20px]">{HeaderText.job}</h2>
           <h2 className="text-not-active font-medium lg:max-w-[50%]">{HeaderText.description}</h2>
         </div>
