3d8f98d1df7ac1b07656c1495b1a3cfd8812f611
kxGeou <grzegorzm.work@gmail.com>
2025-03-11 22:40:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage></MainPage>}></Route>
+        <Route path="/projekty" element={<Projects></Projects>}></Route>
       </Routes>
     </BrowserRouter>
   )
